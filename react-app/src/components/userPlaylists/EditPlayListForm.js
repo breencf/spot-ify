1527944e5310c04da545efcb2a7e3a0effeef714
wilvanson <wilvanson.dutervil@students.cookman.edu>
2022-03-07 23:36:06 -0500

    edit

--- react-app/src/components/userPlaylists/EditPlayListForm.js
+++ react-app/src/components/userPlaylists/EditPlayListForm.js
@@ -2,10 +2,13 @@ import { useDispatch, useSelector } from "react-redux";
 import { useEffect, useState } from "react";
 import { one_Playlists, add_Playlist } from "../../store/playlists";
 import { useHistory, useParams } from "react-router-dom";
+import { Modal } from "../../context/Model";
+import PlaylistsEdit from "./EditPlayList";
 
 const UserPlaylistsEdit = () => {
   const dispatch = useDispatch();
   const { userId, playlistId } = useParams();
+  const [edit, setEdit] = useState(false);
   const history = useHistory();
 
   
@@ -50,42 +53,13 @@ const UserPlaylistsEdit = () => {
   return (
     <div>
       <p>hello from playlists</p>
-      <button>EDIT</button>
+      <button onClick={() => setEdit(true) }>EDIT</button>
       
-      {/* {playLists?.map((list, index) => {
-        return <div key={index}>
-            <p>{list.name}</p>
-        </div>;
-      })}
-      <form onSubmit={handleSubmit}>
-        <ul>
-            {errors?.map((error, index) => {
-                return <li key={index}>{error}</li>
-            })}
-        </ul>
-        <label htmlFor='name'>Name</label>
-        <input
-            onChange={(e) => setName(e.target.value)}
-            value={name}
-            type='text'
-            name='name'
-        />
-        <label htmlFor='image'>ImageUrl</label>
-        <input
-            onChange={(e) => setImage(e.target.value)}
-            value={image}
-            type='text'
-            name='image'
-        />
-        <label htmlFor='description'>Description</label>
-        <input
-            onChange={(e) => setDescription(e.target.value)}
-            value={description}
-            type='text'
-            name='description'
-        />
-        <button type='submit' >Edit Playlist</button>
-      </form> */}
+      {edit && (
+          <Modal onClose={() => setEdit(false)}>
+              <PlaylistsEdit playList={playList} hide={()=> setEdit(false)} />
+          </Modal>
+      )}
     </div>
   );
 };
