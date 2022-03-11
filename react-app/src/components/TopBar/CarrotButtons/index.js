ad88de9b768f80148e7833a2d5c03c592364c339
Chris Breen <breencf@gmail.com>
2022-03-11 10:27:05 -0500

    HOLY SHIT IT WORKS "
    "
    
    """"
    git push
    ;
    "

--- react-app/src/components/TopBar/CarrotButtons/index.js
+++ react-app/src/components/TopBar/CarrotButtons/index.js
@@ -1,20 +1,23 @@
-import { useHistory } from "react-router-dom"
-
-
+import { useHistory } from "react-router-dom";
+import { FaAngleLeft, FaAngleRight } from "react-icons/fa";
 
 export const CarrotButtons = () => {
+  const history = useHistory();
 
-    const history = useHistory()
-
-
-    return (
-        <>
-            <div id="top-left-buttons">
-                <button className="button-green"
-                onClick={(e) => history.goBack()}>-</button>
-                <button className="button-green"
-                onClick={(e) => history.goForward()}>+</button>
-            </div>
-        </>
-    )
-}
+  return (
+    <>
+      <div id="top-left-buttons">
+        <button className="button-none" onClick={(e) => history.goBack()}>
+          <h3>
+            <FaAngleLeft />
+          </h3>
+        </button>
+        <button className="button-none" onClick={(e) => history.goForward()}>
+          <h3>
+            <FaAngleRight />
+          </h3>
+        </button>
+      </div>
+    </>
+  );
+};
