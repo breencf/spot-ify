f8c71adfea859be9f460ee00821a028f3807c2b7
Chris Breen <breencf@gmail.com>
2022-03-13 10:41:52 -0400

    some styling and reworked create user

--- react-app/src/components/TopBar/auth/SignUpForm.js
+++ react-app/src/components/TopBar/auth/SignUpForm.js
@@ -1,32 +1,46 @@
-import React, { useState } from 'react';
-import { useSelector, useDispatch } from 'react-redux'
-import { Redirect } from 'react-router-dom';
-import { signUp } from '../../../store/session';
-import "./Splash.css"
-import {FaSpotify} from 'react-icons/fa'
-import {Link} from 'react-router-dom'
+import React, { useState } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { Redirect } from "react-router-dom";
+import { signUp } from "../../../store/session";
+import "./Splash.css";
+import { FaSpotify } from "react-icons/fa";
+import { Link } from "react-router-dom";
 
 const SignUpForm = () => {
   const [errors, setErrors] = useState([]);
-  const [username, setUsername] = useState('');
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-  const [repeatPassword, setRepeatPassword] = useState('');
-  const user = useSelector(state => state.session.user);
+  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [repeatPassword, setRepeatPassword] = useState("");
+  const [name, setName] = useState("");
+  const user = useSelector((state) => state.session.user);
   const dispatch = useDispatch();
 
   const onSignUp = async (e) => {
     e.preventDefault();
     if (password === repeatPassword) {
-      const data = await dispatch(signUp(username, email, password));
+      let splitName = name.split(" ");
+      const data = await dispatch(
+        signUp({
+          username,
+          email,
+          password,
+          firstName: splitName[0],
+          lastName: splitName[1],
+        })
+      );
       if (data) {
-        setErrors(data)
+        setErrors(data);
       }
     }
   };
 
   const updateUsername = (e) => {
     setUsername(e.target.value);
+  };
+
+  const updateName = (e) => {
+    setName(e.target.value);
   };
 
   const updateEmail = (e) => {
@@ -42,87 +56,79 @@ const SignUpForm = () => {
   };
 
   if (user) {
-    return <Redirect to='/' />;
+    return <Redirect to="/" />;
   }
 
   return (
     <div className="splash">
-      <h1><FaSpotify/> Spot-ify</h1>
-    <div>
-    <form onSubmit={onSignUp}>
+      <h1>
+        <FaSpotify /> Spot-ify
+      </h1>
       <div>
-        {errors.map((error, ind) => (
-          <div key={ind}>{error}</div>
-        ))}
+        <form onSubmit={onSignUp}>
+          <div>
+            {errors.map((error, ind) => (
+              <div key={ind}>{error}</div>
+            ))}
+          </div>
+          <div className="formdiv">
+            <input
+              type="text"
+              name="username"
+              onChange={updateUsername}
+              value={username}
+              placeholder={"Username"}
+            ></input>
+          </div>
+          <div className="formdiv">
+            <input
+              type="text"
+              name="email"
+              onChange={updateEmail}
+              value={email}
+              placeholder={"Email"}
+            ></input>
+          </div>
+          <div className="formdiv">
+            <input
+              type="text"
+              name="firstName"
+              onChange={updateName}
+              value={name}
+              placeholder={"Name(optional)"}
+            ></input>
+          </div>
+          <div className="formdiv">
+            <input
+              type="password"
+              name="password"
+              onChange={updatePassword}
+              value={password}
+              placeholder={"Password"}
+            ></input>
+          </div>
+          <div className="formdiv">
+            <input
+              type="password"
+              name="repeat_password"
+              onChange={updateRepeatPassword}
+              value={repeatPassword}
+              required={true}
+              placeholder={"Confirm Password"}
+            ></input>
+          </div>
+          <div className="formdiv">
+            <button className="button-white" type="submit">
+              Sign Up
+            </button>
+          </div>
+        </form>
+        <div className="formdiv">
+          <Link to="/login">
+            <h4> Login here</h4>
+          </Link>
+        </div>
       </div>
-      <div className='formdiv'>
-        <input
-          type='text'
-          name='username'
-          onChange={updateUsername}
-          value={username}
-          placeholder={"Username"}
-        ></input>
-      </div>
-      <div className='formdiv'>
-        <input
-          type='text'
-          name='email'
-          onChange={updateEmail}
-          value={email}
-          placeholder={"Email"}
-
-        ></input>
-      </div>
-      <div className='formdiv'>
-        <input
-          type='text'
-          name='firstName'
-          // onChange={updateEmail}
-          // value={email}
-          placeholder={"First Name"}
-
-        ></input>
-      </div>
-      <div className='formdiv'>
-        <input
-          type='text'
-          name='lastName'
-          // onChange={updateEmail}
-          // value={email}
-          placeholder={"Last Name"}
-
-        ></input>
-      </div>
-      <div className='formdiv'>
-        <input
-          type='password'
-          name='password'
-          onChange={updatePassword}
-          value={password}
-          placeholder={"Password"}
-
-        ></input>
-      </div>
-      <div className='formdiv'>
-        <input
-          type='password'
-          name='repeat_password'
-          onChange={updateRepeatPassword}
-          value={repeatPassword}
-          required={true}
-          placeholder={"Confirm Password"}
-
-        ></input>
-      </div>
-      <div className='formdiv'>
-      <button className="button-white" type='submit'>Sign Up</button>
-      </div>
-    </form>
-    <div className='formdiv'>
-    <Link to="/login"><h4> Login here</h4></Link>
-    </div>
-    </div>
     </div>
   );
 };
